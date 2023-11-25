ab641d185093784a5b03d01722e65cc250080634
Nikita <nlevin6@uwo.ca>
2023-11-24 19:14:02 -0500

    trying to setup a proxy for my backend

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 
-const SearchForm = ({ onSearch }) => {
+const SearchForm = ({onSearch}) => {
     const [searchParams, setSearchParams] = useState({
         name: '',
         power: '',
         race: '',
-        n: 10, // Default number of searches
+        publisher: '', // Changed from 'n' to 'publisher'
     });
 
+    const [publishers, setPublishers] = useState([]);
+
+    useEffect(() => {
+        fetch('/publishers')
+            .then(response => response.json())
+            .then(data => setPublishers(data))
+            .catch(error => console.error('Error fetching publishers:', error));
+    }, []);
+
     const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setSearchParams((prevParams) => ({ ...prevParams, [name]: value }));
+        const {name, value} = e.target;
+        setSearchParams((prevParams) => ({...prevParams, [name]: value}));
     };
 
     const handleSubmit = (e) => {
@@ -22,19 +31,30 @@ const SearchForm = ({ onSearch }) => {
         <form onSubmit={handleSubmit}>
             <label>
                 Name:
-                <input className="border rounded py-2 px-3" type="text" name="name" value={searchParams.name} onChange={handleInputChange} />
+                <input className="border rounded py-2 px-3" type="text" name="name" value={searchParams.name}
+                       onChange={handleInputChange}/>
             </label>
             <label>
                 Power:
-                <input className="border rounded py-2 px-3" type="text" name="power" value={searchParams.power} onChange={handleInputChange} />
+                <input className="border rounded py-2 px-3" type="text" name="power" value={searchParams.power}
+                       onChange={handleInputChange}/>
             </label>
             <label>
                 Race:
-                <input className="border rounded py-2 px-3" type="text" name="race" value={searchParams.race} onChange={handleInputChange} />
+                <input className="border rounded py-2 px-3" type="text" name="race" value={searchParams.race}
+                       onChange={handleInputChange}/>
             </label>
             <label>
-                Number of Searches:
-                <input className="border rounded py-2 px-3" type="number" name="n" value={searchParams.n} onChange={handleInputChange} />
+                Publisher:
+                <select className="border rounded py-2 px-3" name="publisher" value={searchParams.publisher}
+                        onChange={handleInputChange}>
+                    <option value="">Select a Publisher</option>
+                    {publishers.map((publisher, index) => (
+                        <option key={index} value={publisher}>
+                            {publisher}
+                        </option>
+                    ))}
+                </select>
             </label>
             <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded mb-2">Search</button>
         </form>
