a1aa2b73683f5c07b599055fa52478986d49a54d
Nikita <nlevin6@uwo.ca>
2023-11-26 14:27:41 -0500

    user can login using their credentials only if they verified their email via link sent to their email

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,11 @@
-import React from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
 
 const LoginForm = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleGuestLogin = () => {
         navigate("/superhero-search");
@@ -12,32 +15,74 @@ const LoginForm = () => {
         navigate("/signup");
     }
 
+    const handleLogin = async () => {
+        try {
+            const auth = getAuth();
+
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+
+            if (user && user.emailVerified) {
+                alert('Login successful.');
+                navigate("/superhero-search");//probably give them some privilligies here. so they get more features
+            } else if (user && !user.emailVerified) {
+                alert('Please verify your email before logging in.');
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     return (
         <div className="text-center">
             <h1 className="text-3xl font-bold mb-6">Login</h1>
 
             <form className="flex flex-col items-center justify-center">
                 <div className="mb-4 flex items-center">
-                    <label htmlFor="username" className="mb-2">Username:</label>
-                    <input type="text" id="username" name="username" className="border rounded py-2 px-3"/>
+                    <label htmlFor="email" className="mb-2">Email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        className="border rounded py-2 px-3"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
 
                 <div className="mb-4 flex items-center">
                     <label htmlFor="password" className="mb-2">Password:</label>
-                    <input type="password" id="password" name="password" className="border rounded py-2 px-3"/>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="border rounded py-2 px-3"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
 
-                <button type="button" className="bg-green-500 text-white py-2 px-4 rounded mb-2">
+                <button
+                    type="button"
+                    className="bg-green-500 text-white py-2 px-4 rounded mb-2"
+                    onClick={handleLogin}
+                >
                     Login
                 </button>
 
-                <button type="button" className="bg-blue-500 text-white py-2 px-4 rounded mb-2"
-                onClick={handleSignup}>
+                <button
+                    type="button"
+                    className="bg-blue-500 text-white py-2 px-4 rounded mb-2"
+                    onClick={handleSignup}
+                >
                     Sign Up
                 </button>
 
-                <button type="button" className="bg-gray-500 text-white py-2 px-4 rounded mb-2"
-                        onClick={handleGuestLogin}>
+                <button
+                    type="button"
+                    className="bg-gray-500 text-white py-2 px-4 rounded mb-2"
+                    onClick={handleGuestLogin}
+                >
                     Guest Login
                 </button>
             </form>
