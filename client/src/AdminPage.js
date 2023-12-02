3234ca5b377176eb8edacf30f4796819d5ee9a01
Nikita <nlevin6@uwo.ca>
2023-12-01 19:39:55 -0500

    adminPage now functioning for admins only

--- client/src/AdminPage.js
+++ client/src/AdminPage.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import AdminForm from "./components/AdminForm";
 
 const AdminPage = () => {
     const navigate = useNavigate();
     const [userRole, setUserRole] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const auth = getAuth();
@@ -16,21 +17,30 @@ const AdminPage = () => {
             } else {
                 setUserRole(null);
             }
+            setLoading(false);
         });
 
         return () => unsubscribe();
     }, []);
 
     useEffect(() => {
-        if (userRole !== 'admin') {
+        if (!loading && userRole !== 'admin') {
             alert("You are not authorized to view this page.");
             navigate('/superhero-search');
         }
-    }, [userRole, navigate]);
+    }, [loading, userRole, navigate]);
 
     return (
         <div className="text-center">
-            <AdminForm/>
+            <Link to="/superhero-search" className="absolute top-0 left-0 m-4">
+                <button className="bg-blue-500 text-white py-2 px-4 rounded">Back</button>
+            </Link>
+
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                userRole === 'admin' && <AdminForm />
+            )}
         </div>
     );
 };
